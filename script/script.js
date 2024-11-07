d1649659a235e99a51eb3edaed0d690d99e1de16
FlaashTT <pintoleandro331@gmail.com>
2024-11-07 12:04:27 +0000

    Update

--- script/script.js
+++ script/script.js
@@ -1,31 +1,29 @@
-let btnCategorias, categorias, textoNome;
+let btnCategorias, categorias, textoNome, sidebar, iconConta, iconFavs, iconCarrinho, searchBox, searchInput;
 
 window.onload = function () {
     userLogged = JSON.parse(localStorage.getItem("userLogged"));
-    categorias = document.getElementById("categorias");
-
+    sidebar = document.getElementById("sidebar");
     iconConta = document.getElementById("iconConta");
     btnCategorias = document.getElementById("btnCategorias");
     textoNome = document.getElementById("textoNome");
     iconFavs = document.getElementById("iconFavs");
     iconCarrinho = document.getElementById("iconCarrinho");
+    
+    // Corrige o seletor da search-box e searchInput
+    searchBox = document.querySelector('.search-box');
+    searchInput = document.querySelector('.search-text');
+
     header();
 }
 
 function header() {
-    if(userLogged !=null){
-        textoNome.innerHTML = "Olá <br>" + userLogged.name + " !";
-    }else{
-        textoNome.innerHTML = "Erro!"
+    if (userLogged != null) {
+        textoNome.innerHTML = "Olá <br>" + userLogged.name + "!";
+    } else {
+        textoNome.innerHTML = "Erro!";
     }
-   
-    btnCategorias.addEventListener("click", function () {
-        if (categorias.classList.contains("hidden")) {
-            categorias.classList.remove("hidden");
-        } else {
-            categorias.classList.add("hidden");
-        }
-    });
+
+    btnCategorias.addEventListener("click", toggleSidebar);
 
     iconConta.addEventListener("click", function () {
         window.location.href = '../html/conta.html';
@@ -34,9 +32,23 @@ function header() {
     iconFavs.addEventListener("click", function () {
         window.location.href = '../html/favoritos.html';
     });
-    iconCarrinho.addEventListener("click",function(){
+
+    iconCarrinho.addEventListener("click", function () {
         window.location.href = '../html/carrinho.html';
-    })
+    });
 
+    searchInput.addEventListener('focus', function() {
+        searchBox.classList.add('expanded'); 
+    });
 
+    searchInput.addEventListener('blur', function() {
+        if (!searchInput.value) { 
+            searchBox.classList.remove('expanded');
+        }
+    });
+}
+
+function toggleSidebar() {
+    sidebar.classList.toggle("hidden");
+    sidebar.classList.toggle("show");
 }
