ee679c62fa21bbf9b1c58645734d03b447c52231
FlaashTT <pintoleandro331@gmail.com>
2024-11-15 13:16:56 +0000

    Update

---
+++ script/book.js
@@ -0,0 +1,60 @@
+let btnCategorias, categorias, textoNome, sidebar, iconConta, iconFavs, iconCarrinho, searchBox, searchInput;
+
+window.onload = function () {
+    // Recupera o usuário logado do localStorage
+    let userLogged = JSON.parse(localStorage.getItem("userLogged"));
+
+    // Seletores dos elementos da página
+    sidebar = document.getElementById("sidebar");
+    iconConta = document.getElementById("iconConta");
+    btnCategorias = document.getElementById("btnCategorias");
+    textoNome = document.getElementById("textoNome");
+    iconFavs = document.getElementById("iconFavs");
+    iconCarrinho = document.getElementById("iconCarrinho");
+    
+    // Corrige o seletor da search-box e searchInput
+    searchBox = document.querySelector('.search-box');
+    searchInput = document.querySelector('.search-text');
+
+    header(userLogged);
+}
+
+function header(userLogged) {
+    if (userLogged != null) {
+        textoNome.innerHTML = "Olá <br>" + userLogged.name + " !";
+    } else {
+        textoNome.innerHTML = "Olá <br> anónimo !";
+    }
+
+    btnCategorias.addEventListener("click", function(){
+        sidebar.classList.toggle("hidden");
+        sidebar.classList.toggle("show");
+    });
+
+    iconConta.addEventListener("click", function () {
+        if (userLogged != null) {
+            window.location.href = '../html/definicoes.html';
+        } else {
+            window.location.href = '../html/registo.html';
+        }
+    });
+
+    iconFavs.addEventListener("click", function () {
+        window.location.href = '../html/favoritos.html';
+    });
+
+    iconCarrinho.addEventListener("click", function () {
+        window.location.href = '../html/carrinho.html';
+    });
+
+    searchInput.addEventListener('focus', function() {
+        searchBox.classList.add('expanded');
+    });
+
+    searchInput.addEventListener('blur', function() {
+        if (!searchInput.value) {
+            searchBox.classList.remove('expanded');
+        }
+    });
+}
+
